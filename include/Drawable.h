dfd6db4edb5c0221e3c0b4a64dc74d40e52345c2
Janne Välimaa <janne.valimaa@gmail.com>
2012-03-16 11:45:32 +0200

    Added drawable-class

---
+++ include/Drawable.h
@@ -0,0 +1,36 @@
+#ifndef DRAWABLE_H
+#define DRAWABLE_H
+
+#include <SFML/Graphics.hpp>
+
+
+class Drawable
+{
+    public:
+        Drawable();
+        virtual ~Drawable();
+        void Draw(sf::RenderWindow &window);
+        int GetArea();
+
+        /* Getters */
+        float GetXpos() { return posx; }
+        float GetYpos() { return posy; }
+        float GetAccel(){ return accel;}
+        float GetRotation() { return rotationAngle; }
+        float GetWidth(){ return sprite.GetSize().x;}
+        float GetHeight(){ return sprite.GetSize().y;}
+
+    protected:
+    private:
+        sf::Image image;
+        sf::Sprite sprite;
+        sf::Clock spawnTime;
+        float posx;
+        float posy;
+        float accel;
+        float speedx;
+        float speedy;
+        float rotationAngle;
+};
+
+#endif // DRAWABLE_H
---
+++ src/Drawable.cpp
@@ -0,0 +1,35 @@
+#include "Drawable.h"
+#include "Misc.h
+
+Drawable::Drawable()
+{
+}
+
+Drawable::~Drawable()
+{
+}
+
+Drawable::Draw()
+{
+    window.Draw(sprite);
+}
+
+int Drawable::GetArea()
+{
+    if(posx < WIN_WIDTH / 2 && posy < WIN_HEIGHT / 2)
+    {
+        return 0;
+    }
+    else if(posx > WIN_WIDTH / 2 && posy < WIN_HEIGHT / 2)
+    {
+        return 1;
+    }
+    else if(posx < WIN_WIDTH / 2 && posy > WIN_HEIGHT / 2)
+    {
+        return 2;
+    }
+    else
+    {
+        return 3;
+    }
+}
