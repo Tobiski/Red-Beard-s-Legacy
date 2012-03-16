59e88b6d16b5879a81d2fb653fd07d460d1b292c
Topi Stolp <topi_stolp@hotmail.com>
2012-03-16 14:14:32 +0200

    ShipEntity muutoksia

--- src/ShipEntity.cpp
+++ src/ShipEntity.cpp
@@ -5,7 +5,7 @@
 bool ShipEntity::CheckCollision(ShipEntity &enemy)
 {
     if(posx + sprite.GetSize().x > enemy.GetXpos() - (enemy.GetWidth()/2) &&
-            posx< enemy.GetXpos() + enemy.GetWidth() - (enemy.GetWidth()/2) &&
+            posx < enemy.GetXpos() + enemy.GetWidth() - (enemy.GetWidth()/2) &&
             posy + sprite.GetSize().y > enemy.GetYpos() - (enemy.GetHeight()/2) &&
             posy < enemy.GetYpos() + enemy.GetHeight() - (enemy.GetHeight()/2))
     {
